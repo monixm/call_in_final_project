e2e8ff6d41dc8a8f98d394f8fdb0dcd788c50fdd
gianniguida <gianni.guida@icloud.com>
2019-11-14 11:40:25 +0100

    Temporary fix so that the CallSerializer doesn't give problem with import in the feed app

--- backend/project/api/feed/serializers.py
+++ backend/project/api/feed/serializers.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 from project.api.organisation.serializer import OrganisationSerializer
 from project.api.volunteer.serializer import VolunteerSerializer
 
+from project.api.call_option.serializer import CallOptionSerializer
+from project.api.call.models import Call
+
 
 class ReadMeSerializer(serializers.ModelSerializer):
     organisation = OrganisationSerializer()
@@ -12,3 +15,14 @@ class ReadMeSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'organisation', 'volunteer']
+
+
+class CallFeedSerializer(serializers.ModelSerializer):
+    call_options = CallOptionSerializer(many=True, required=False)
+
+    class Meta:
+        model = Call
+        fields = ['title', 'call_picture', 'organisation', 'start_datetime', 'end_datetime',
+                  'location', 'description', 'must_be_approved', 'call_options']
+        read_only_fields = ['organisation']
+        ordering = ['start_datetime']
